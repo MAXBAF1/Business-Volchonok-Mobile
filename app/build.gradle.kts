4fb091e0a5b1a110153d0eb9ca43be2fe6ff6a5f
nikita-popov-java <nikitapopov1295@gmail.com>
2023-10-27 16:10:19 +0500

    changed java version from 8 to 17
    Signed-off-by: nikita <nikitapopov1295@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,11 +30,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
@@ -71,4 +71,6 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    // Service
+    implementation("com.google.code.gson:gson:2.10.1")
 }
